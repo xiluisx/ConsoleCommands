e2d1c8b76dc635ff647af5fc9d9aa13ad1d5d8f3
xiluisx <xiluisx1234@gmail.com>
2020-07-16 23:01:48 -0500

    Moved commands and added Clear

---
+++ ConsoleCommands/Commands/Clear.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+namespace ConsoleCommands
+{
+    class Clear : Command
+    {
+        public override string command => "clear";
+
+        public override string Name => "Clear";
+
+        public override string Description => "Clears the console messages.";
+
+        public override void Run(string[] args)
+        {
+            Console.Clear();
+            Debug.LogWarning("Cleared console.");
+        }
+    }
+}
---
+++ ConsoleCommands/Commands/Moderation/GetUserInfo.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+namespace ConsoleCommands.Commands.Moderation
+{
+    class GetUserInfo : Command
+    {
+        public override string command => "getuser";
+
+        public override string Name => "GetUserInfo";
+
+        public override string Description => "Gets the information of an user.";
+
+        public override void Run(string[] args)
+        {
+            if(Core.Instance.SvManager.database.Users.Exists(args[0]))
+            {
+                var user = Core.Instance.SvManager.database.Users.FindById(args[0]);
+                Debug.Log(user.ID);
+                Debug.Log(user.JoinDate);
+                Debug.Log(user.Character);
+            }
+        }
+    }
+}
